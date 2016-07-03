0871bcaa10537fafe831fa02c7c50e58d6d187e6
Sathyanarayanan Kuppuswamy <sathya.kernel@gmail.com>
2016-07-03 00:36:10 -0700

    parser: Add duplicate check support
    Signed-off-by: Sathyanarayanan Kuppuswamy <sathya.kernel@gmail.com>

--- essence_file_reader.py
+++ essence_file_reader.py
@@ -30,6 +30,14 @@ def format_ccomment(pstr):
 
     return out_str
 
+def format_cdefine(macro_name, macro_val, width=default_macro_width):
+    macro = "#define "
+    macro += macro_name
+    macro = macro.ljust(width)
+    macro += macro_val
+
+    return macro
+
 def unique_list(ilist):
     olist = []
     for item in ilist:
@@ -140,10 +148,9 @@ class EnumElementNode(object):
                 "long desc  : " + self.long_description + "\n" + \
                 "hidden     : " + str(self.hidden) + "\n"
 
-    def write_cdefine(self, filep, width=default_macro_width, tag=""):
+    def get_cdefine(self, tag=""):
         if self.name is not None and self.value is not None:
             macro_val = self.value
-            macro = "#define "
             macro_name = []
             if tag != "":
                 macro_name += tag.split('_')
@@ -155,20 +162,19 @@ class EnumElementNode(object):
             # handling some special cases
             if len(macro_name) > 0 and  macro_name[-1] == "MASK":
                 macro_name[-1] = "VALUE"
-            #macro += "_".join(unique_list(macro_name))
-            macro += "_".join(macro_name)
-            macro =  macro.ljust(width)
-            filep.write(macro)
-            filep.write(macro_val)
-            filep.write("\n")
+            macro_name = "_".join(macro_name)
+            return macro_name, macro_val
 
-    def write_ccomment(self, filep, tag=""):
+    def get_ccomment(self, filep, tag=""):
         if self.short_description is not None and self.name is not None:
             comment = self.name
             comment += " : "
             comment += self.short_description
-            filep.write(format_ccomment(comment))
-            filep.write("\n")
+            return comment
+
+    def print_info(self):
+        print "Enum Field Name :" + self.name
+        self.parent.print_info()
 
   
 class BitFieldElement(object):
@@ -200,37 +206,34 @@ class BitFieldElement(object):
                 "long desc  : " + self.long_description + "\n" + \
                 "hidden     : " + str(self.hidden) + "\n"
 
-    def write_cdefine(self, filep, width=default_macro_width, tag=""):
+    def get_cdefine(self, tag=""):
         if self.name is not None and self.offset is not None and self.width is not None:
             mask = hex(pow(2, int(self.width)) - 1)
-            macro = "#define "
             macro_name = []
             if tag != "":
                 macro_name += tag.split('_')
             if self.tag is not None:
                 macro_name += self.tag.split('_')
-            #macro_name += self.name.split('_')
             macro_name  += remove_dup(macro_name, self.name.split('_'))
             macro_name.append("MASK")
-            macro += "_".join(macro_name)
-            #macro += "_".join(unique_list(macro_name))
-            macro =  macro.ljust(width)
+            macro_name = "_".join(macro_name)
             if int(self.offset) == 0:
                 macro_val = str(mask)
             else:
                 macro_val = "( " + str(mask) + " << " + self.offset + " )"
 
-            filep.write(macro)
-            filep.write(macro_val)
-            filep.write("\n")
+            return macro_name, macro_val
 
-    def write_ccomment(self, filep, tag=""):
+    def get_ccomment(self, filep, tag=""):
         if self.short_description is not None and self.name is not None:
             comment = self.name
             comment += " : "
             comment += self.short_description
-            filep.write(format_ccomment(comment))
-            filep.write("\n")
+            return comment
+
+    def print_info(self):
+        print "Bit Field Name :" + self.name
+        self.parent.print_info()
 
 
 class RegisterMemElement(object):
@@ -269,26 +272,26 @@ class RegisterMemElement(object):
                 "long desc  : " + self.long_description + "\n" + \
                 "hidden     : " + str(self.hidden) + "\n"
 
-    def write_cdefine(self, filep, width=default_macro_width, tag=""):
+    def get_cdefine(self, tag=""):
         if self.name is not None and self.offset is not None:
             macro_val = hex(int(self.offset))
-            macro = "#define "
+            macro_name = ""
             if tag != "":
-                macro += tag
-                macro += "_"
-            macro += self.name
-            macro =  macro.ljust(width)
-            filep.write(macro)
-            filep.write(macro_val)
-            filep.write("\n")
+                macro_name += tag
+                macro_name += "_"
+            macro_name += self.name
+            return macro_name, macro_val
 
-    def write_ccomment(self, filep, tag=""):
+    def get_ccomment(self, filep, tag=""):
         if self.short_description is not None and self.name is not None:
             comment = self.name
             comment += " : "
             comment += self.short_description
-            filep.write(format_ccomment(comment))
-            filep.write("\n")
+            return comment
+
+    def print_info(self):
+        print "Register Name :" + self.name
+        self.parent.print_info()
 
 
 class RegisterMemSet(object):
@@ -321,14 +324,15 @@ class RegisterMemSet(object):
                 "long desc  : " + self.long_description + "\n" + \
                 "hidden     : " + str(self.hidden) + "\n"
 
-    def write_ccomment(self, filep, tag=""):
+    def get_ccomment(self, filep, tag=""):
         if self.short_description is not None and self.name is not None:
             comment = self.name
             comment += " : "
             comment += self.short_description
-            filep.write(format_ccomment(comment))
-            filep.write("\n")
+            return comment
 
+    def print_info(self):
+        print "RegMemSetName    :" + self.name
 
 class EssenceFileReader(object):
 
@@ -336,6 +340,7 @@ class EssenceFileReader(object):
         self.essence_file_xml = essence_file_xml
         self._xml_root = xml.etree.ElementTree.parse(essence_file_xml).getroot()
         self.regmem_sets = []
+        self.macro_sets = []
 
         # generate regmem sets
         xml_regmem_sets = self._xml_root.findall(RegisterMemSet.element_name)
@@ -343,18 +348,65 @@ class EssenceFileReader(object):
             regmem_set = RegisterMemSet(self._xml_root, xml_regmem_set)
             self.regmem_sets.append(regmem_set)
 
+    def _fix_macro_dup(self, max_index, cur_index, input_str):
+        while True:
+            new_macro_name = raw_input(input_str)
+            is_duplicate = False
+            for j in range(max_index):
+                if self.macro_sets[j][2] is not None and self.macro_sets[j][2].strip() == new_macro_name:
+                    is_duplicate = True
+            if not is_duplicate:
+                temp_set = list(self.macro_sets[cur_index])
+                temp_set[2] = new_macro_name
+                self.macro_sets[cur_index] = tuple(temp_set)
+                break
+            else:
+                print "Duplicate entry found, enter new name"
+
+    def _check_for_macro_dup(self):
+        index = 0
+        print "Number of lines: " + str(len(self.macro_sets))
+        for macro_set in self.macro_sets:
+            index += 1
+            if macro_set[2] is not None:
+                for i in range(index-1):
+                    if self.macro_sets[i][2] is not None and self.macro_sets[i][2].strip() == macro_set[2].strip():
+                        print "##########################################"
+                        print "Found duplicate"
+                        print "original: " + self.macro_sets[i][2]
+                        self.macro_sets[i][0].print_info()
+                        print "new: " + macro_set[2]
+                        macro_set[0].print_info()
+                        print "##########################################"
+                        self._fix_macro_dup(index - 1, i, "Enter new macro name for original entry:")
+                        self._fix_macro_dup(index - 1, self.macro_sets.index(macro_set), "Enter new macro name for new entry:")
+
     def generate_header_file(self, outfile, product_tag):
         for regmem_set in self.regmem_sets:
-            regmem_set.write_ccomment(outfile)
+            comment = regmem_set.get_ccomment(outfile)
+            self.macro_sets.append((regmem_set, comment, None, None))
             for regmem_element in regmem_set.regmem_elements:
-                regmem_element.write_ccomment(outfile)
-                regmem_element.write_cdefine(outfile, tag=product_tag)
+                comment = regmem_element.get_ccomment(outfile)
+                macro, macro_val = regmem_element.get_cdefine(tag=product_tag)
+                self.macro_sets.append((regmem_element, comment, macro, macro_val))
                 for bit_field_element in regmem_element.bit_field_elements:
-                    bit_field_element.write_ccomment(outfile)
-                    bit_field_element.write_cdefine(outfile)
+                    comment = bit_field_element.get_ccomment(outfile)
+                    macro, macro_val = bit_field_element.get_cdefine()
+                    self.macro_sets.append((bit_field_element, comment, macro, macro_val))
                     for enum_element in bit_field_element.enum_elements:
-                        #enum_element.write_ccomment(outfile)
-                        enum_element.write_cdefine(outfile)
+                        #enum_element.get_ccomment(outfile)
+                        macro, macro_val = enum_element.get_cdefine()
+                        self.macro_sets.append((enum_element, None, macro, macro_val))
+
+        self._check_for_macro_dup()
+
+        for macroset in self.macro_sets:
+            if macroset[1] is not None:
+                outfile.write(format_ccomment(macroset[1]))
+                outfile.write("\n")
+            if macroset[2] is not None and macroset[3] is not None:
+                outfile.write(format_cdefine(macroset[2], macroset[3]))
+                outfile.write("\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
